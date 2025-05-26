6cdabb67928b1bec85bcc2fcc151611d59830aa6
Moussa-codeur <correamoussa469@gmail.com>
2025-05-26 14:05:31 +0200

    création du model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    is_done = models.BooleanField(default=False)
+    position = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['position']
+
+    def __str__(self):
+        return self.title
